85975ed1ce944f0178a01392ad676c4275cb3646
saul colima <saulinho.smc51@gmail.com>
2024-04-14 03:03:30 -0500

    Feat: Actualizacion de vista de registros de usuario.

--- src/main/java/com/microcontrollersystem/wirelessrfidfrontend/controllers/IndexController.java
+++ src/main/java/com/microcontrollersystem/wirelessrfidfrontend/controllers/IndexController.java
@@ -1,13 +1,18 @@
 package com.microcontrollersystem.wirelessrfidfrontend.controllers;
 
 import com.microcontrollersystem.wirelessrfidfrontend.configuration.SystemProperties;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Objects;
+
 @Controller
 @AllArgsConstructor
+@Slf4j
 public class IndexController {
 
     private final SystemProperties systemProperties;
@@ -16,15 +21,25 @@ public class IndexController {
     public String loginView(Model model) {
         model.addAttribute("url", systemProperties.getUrl());
         model.addAttribute("icono", systemProperties.getIconUrl());
-        model.addAttribute("title", "SCM - Index");
+        model.addAttribute("title", "SCM - Login");
         return "/html/loginView";
     }
 
     @GetMapping(value = "/index")
-    public String indexView(Model model) {
+    public String indexView(Model model, HttpSession httpSession) {
         model.addAttribute("url", systemProperties.getUrl());
         model.addAttribute("icono", systemProperties.getIconUrl());
         model.addAttribute("title", "SCM - Index");
+        Object tokenRetrieve = httpSession.getAttribute("token");
+
+        tokenRetrieve = "admin";
+        if (Objects.isNull(tokenRetrieve)) {
+            return "redirect:login";
+        }
+        log.info(tokenRetrieve.toString());
+
+        model.addAttribute("user", "admin");
+
         return "/html/indexView";
     }
 }
