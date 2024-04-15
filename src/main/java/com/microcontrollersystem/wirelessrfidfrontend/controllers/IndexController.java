be379b4ffefe9994fde9bb325dbac558aaee0c86
saul colima <saulinho.smc51@gmail.com>
2024-04-14 23:12:34 -0500

    Feat: Mapeo de tabla de usuarios.

--- src/main/java/com/microcontrollersystem/wirelessrfidfrontend/controllers/IndexController.java
+++ src/main/java/com/microcontrollersystem/wirelessrfidfrontend/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.microcontrollersystem.wirelessrfidfrontend.controllers;
 
+import com.google.gson.Gson;
 import com.microcontrollersystem.wirelessrfidfrontend.configuration.SystemProperties;
 import com.microcontrollersystem.wirelessrfidfrontend.models.dto.TokenData;
 import com.microcontrollersystem.wirelessrfidfrontend.models.dto.UserData;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -36,7 +38,7 @@ public class IndexController {
     public String indexView(Model model, HttpSession httpSession) {
         model.addAttribute("url", systemProperties.getUrl());
         model.addAttribute("icono", systemProperties.getIconUrl());
-        model.addAttribute("title", "SCM - Index");
+
         try {
             TokenData token = loginService.validateToken(httpSession);
             if (Objects.isNull(token)) {
@@ -45,6 +47,7 @@ public class IndexController {
             Object tokenRetrieve = httpSession.getAttribute("token");
             String tokenRetrieveString = tokenRetrieve.toString();
             model.addAttribute("user", token.getSub());
+
             List<UserData> userDataList = userService.getUserList( tokenRetrieveString);
             model.addAttribute("listUser",userDataList);
             return "/html/indexView";
